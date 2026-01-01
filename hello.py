a7db8c75c967771b828c8380508d15b0a387cb8a
youssefDEV <youssefredmi599@gmail.com>
2026-01-01 16:52:04 +0100

    Ajout du fichier hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+
+
+# Demander le nom de l'utilisateur
+name = input("Quel est votre nom ? ")
+
+# Afficher un message de bienvenue
+print(f"Bonjour, {name} ! Bienvenue dans le programme Hello.py")
+
+# Afficher un petit message supplémentaire
+print("Passez une excellente journée !")
